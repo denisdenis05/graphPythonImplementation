3eddda7fb91e395e055632759258286914ae9f8a
denisdenis05 <denisgre205@gmail.com>
2024-03-21 01:29:27 +0200

    specifications

--- graphDomain.py
+++ graphDomain.py
@@ -8,13 +8,32 @@ class Graph:
         self.latestGeneratedEdgeId = 0
 
     def overwriteNodes(self, newNodesList):
+        """
+        Overwrites the existing list of nodes with a new list provided as an argument
+        :param newNodesList: list of nodes to overwrite
+        :return:
+        """
         self.__nodes = newNodesList
     def overwriteEdges(self, newEdgesList):
+        """
+        Overwrites the existing dictionary of edges with a new dictionary provided as an argument
+        :param newEdgesList: dictionary of edges to overwrite
+        :return:
+        """
         self.__edges = newEdgesList
     def overwriteEdgeCosts(self, newEdgesCostList):
+        """
+        Overwrites the existing dictionary of edge costs with a new dictionary provided as an argument
+        :param newEdgesCostList: dictionary of edges of costs to overwrite
+        :return:
+        """
         self.__edgesCosts = newEdgesCostList
 
     def copyGraph(self):
+        """
+        Creates a deep copy of the graph object and returns it
+        :return: a copy of the graph, type Graph
+        """
         newNodesList = deepcopy(self.__nodes)
         newEdgesList = deepcopy(self.__edges)
         newEdgesCostsList = deepcopy(self.__edgesCosts)
@@ -26,6 +45,11 @@ class Graph:
 
 
     def createNode(self, nodeId):
+        """
+        Adds a new node to the graph with the specified node ID if it does not already exist
+        :param nodeId: node to add to the graph
+        :return: True if the node was added, False if already added
+        """
         if nodeId not in self.__nodes:
             self.__nodes.append(nodeId)
             return True
@@ -33,6 +57,11 @@ class Graph:
             return False
 
     def removeNode(self, nodeId):
+        """
+        Removes the node with the specified ID from the graph, along with any incident edges
+        :param nodeId: node to remove from the graph
+        :return: True if node was removed, False if not in graph
+        """
         if nodeId in self.__nodes:
             for nodeNr in range(len(self.__nodes)):
                 if self.__nodes[nodeNr] == nodeId:
@@ -46,11 +75,22 @@ class Graph:
 
 
     def checkIfNodeExists(self, nodeId):
+        """
+        Checks if a node with the specified ID exists in the graph
+        :param nodeId: node id to check if in the graph
+        :return: True if node is in the graph, False otherwise
+        """
         if nodeId not in self.__nodes:
             return False
         return True
 
     def checkIfExistsEdgeFromNodeToNode(self, node1, node2):
+        """
+        Checks if an edge exists between two specified nodes
+        :param node1: source node
+        :param node2: destination node
+        :return: the Edge ID if found, -1 if does not exist
+        """
         for edgeId in self.__edges:
             if self.__edges[edgeId][0] == node1 and self.__edges[edgeId][1] == node2:
                 return edgeId
@@ -59,6 +99,14 @@ class Graph:
 
 
     def addEdge(self, source, target, edgeId, cost=0):
+        """
+        Adds a new edge to the graph with the specified source and target nodes, edge ID, and optionally, cost
+        :param source: the source node
+        :param target: the destination node
+        :param edgeId: the given edge id
+        :param cost: the cost of the edge (optional, will be set as 0 if not specified)
+        :return: True if edge was added, False if edge from source to destination already exists
+        """
         if self.checkIfExistsEdgeFromNodeToNode(source, target) != -1:
             return False
         self.__edges[edgeId] = (source, target)
@@ -66,6 +114,11 @@ class Graph:
         return True
 
     def removeEdgeById(self, edgeId):
+        """
+        Removes the edge with the specified ID from the graph
+        :param edgeId: the edge ID
+        :return: True if the edge was removed, False if not in graph
+        """
         if edgeId not in self.__edges:
             return False
         self.__edges.pop(edgeId)
@@ -73,6 +126,12 @@ class Graph:
         return True
 
     def removeEdgeByNodes(self, source, destination):
+        """
+        Removes the edge between the specified source and destination nodes from the graph
+        :param source: the source node
+        :param destination: the destination node
+        :return: True if the edge was removed, False if not in graph
+        """
         edgeId = self.checkIfExistsEdgeFromNodeToNode(source, destination)
         if edgeId == -1:
             return False
@@ -82,31 +141,56 @@ class Graph:
 
 
     def setEdgeCost(self, edgeId, cost):
+        """
+        Sets the cost of the edge with the specified ID to the provided cost value
+        :param edgeId: the edge ID
+        :param cost: the cost to set
+        :return: None
+        """
         self.__edgeCosts[edgeId] = cost
 
-    def getNodes(self):
-        return self.__nodes
-
     def getNumberOfNodes(self):
+        """
+        Returns the total number of nodes in the graph
+        :return: the total number of nodes
+        """
         return len(self.__nodes)
 
     def getNumberOfEdges(self):
+        """
+        Returns the total number of edges in the graph
+        :return: the total number of edges
+        """
         return len(self.__edges)
 
-    def getAllEdges(self):
-        return list(self.__edges)
 
     def getEdgeEndpoints(self, edgeId):
+        """
+        Returns a tuple containing the source and destination nodes of the edge with the specified ID.
+        :param edgeId: the edge ID
+        :return: a tuple (source, destination)
+        """
         if edgeId not in self.__edges:
             return None
         return self.__edges[edgeId]
 
-    def hasEdge(self, source, target):
-        if self.checkIfExistsEdgeFromNodeToNode(source, target) == -1:
+    def hasEdge(self, source, destination):
+        """
+        Checks if there exists an edge between the specified source and target nodes
+        :param source: the source node
+        :param destination: the destination node
+        :return: the edge ID if found, False otherwise
+        """
+        if self.checkIfExistsEdgeFromNodeToNode(source, destination) == -1:
             return False
-        return self.checkIfExistsEdgeFromNodeToNode(source, target)
+        return self.checkIfExistsEdgeFromNodeToNode(source, destination)
 
     def getOutDegree(self, source):
+        """
+        Returns the outer degree of the specified node
+        :param source: the node too check for its degree
+        :return: the node outer degree
+        """
         degree = 0
         for edgeId in self.__edges:
             if self.__edges[edgeId][0] == source:
@@ -114,6 +198,11 @@ class Graph:
         return degree
 
     def getInDegree(self, destination):
+        """
+        Returns the inner degree of the specified node
+        :param destination: the node too check for its degree
+        :return: the node inner degree
+        """
         degree = 0
         for edgeId in self.__edges:
             if self.__edges[edgeId][1] == destination:
@@ -121,6 +210,11 @@ class Graph:
         return degree
 
     def getOutEdges(self, source):
+        """
+        Returns a dictionary containing all outgoing edges from the specified source node, with edge IDs as keys and endpoints as values
+        :param source: the node too check for its outgoing edges
+        :return: a dictionary of outgoing edges
+        """
         outEdges = {}
         for edgeId in self.__edges:
             if self.__edges[edgeId][0] == source:
@@ -128,6 +222,11 @@ class Graph:
         return outEdges
 
     def getInEdges(self, destination):
+        """
+        Returns a dictionary containing all incoming edges to the specified destination node, with edge IDs as keys and endpoints as values
+        :param destination: the node too check for its incoming edges
+        :return: a dictionary of incoming edges
+        """
         inEdges = {}
         for edgeId in self.__edges:
             if self.__edges[edgeId][1] == destination:
@@ -136,10 +235,21 @@ class Graph:
 
 
     def getEdgeIdPrice(self, edgeId):
+        """
+        Returns the cost associated with the edge specified by the given edge ID
+        :param edgeId: the edge ID
+        :return: the cost of the edge
+        """
         return self.__edgeCosts[edgeId]
 
 
     def getEdgeNodesPrice(self, source, destination):
+        """
+        Returns the edge ID and cost associated with the edge between the specified source and destination nodes
+        :param source: the source node
+        :param destination: the destination node
+        :return: the cost of the edge
+        """
         edgeId = self.checkIfExistsEdgeFromNodeToNode(source, destination)
         if edgeId == -1:
             return False
@@ -147,19 +257,37 @@ class Graph:
 
 
     def parseNodes(self):
+        """
+        Generator function that yields each node in the graph
+        :return: an iterator with each node in the graph
+        """
         for nodeId in self.__nodes:
             yield nodeId
 
     def parseEdges(self):
+        """
+        Generator function that yields each edge ID in the graph
+        :return: an iterator with each edge in the graph
+        """
         for edge in self.__edges:
             yield edge
 
     def parseOutboundEdges(self, nodeId):
+        """
+        Generator function that yields each outbound edge ID from the specified node
+        :param nodeId: the node ID
+        :return: an iterator with each outbound edge for the specified node
+        """
         for edgeId in self.__edges:
             if self.__edges[edgeId][0] == nodeId:
                 yield edgeId
 
     def parseInboundEdges(self, nodeId):
+        """
+        Generator function that yields each inbound edge ID to the specified node
+        :param nodeId: the node ID
+        :return: an iterator with each inbound edge for the specified node
+        """
         for edgeId in self.__edges:
             if self.__edges[edgeId][1] == nodeId:
                 yield edgeId
--- start.py
+++ start.py
@@ -4,6 +4,12 @@ from graphDomain import Graph
 
 
 def parseFile(filename, graph):
+    """
+    Reads data from a file and populates the graph with nodes and edges
+    :param filename: the name of the file to read from
+    :param graph: the graph to populate
+    :return: None
+    """
     with open(filename, 'r') as file:
         lines = file.readlines()
         numberOfVertices,numberOfEdges = lines[0].split(" ")
@@ -18,6 +24,13 @@ def parseFile(filename, graph):
 
 
 def createRandomGraphFile(filename, numberOfNodes, numberOfEdges):
+    """
+    Generates a random graph and writes it to a file
+    :param filename: the name of the file to write to
+    :param numberOfNodes: the number of nodes to create
+    :param numberOfEdges: the number of edges to create
+    :return: None
+    """
     alreadyExistingNodes = []
     addedEdges = 0
     textToWrite = F"{numberOfNodes} {numberOfEdges}\n"
@@ -41,6 +54,12 @@ def createRandomGraphFile(filename, numberOfNodes, numberOfEdges):
 
 
 def writeGraphToFile(graph, filename):
+    """
+    Writes the details of the graph into a file
+    :param graph: the graph to write to file
+    :param filename: the name of the file to write to
+    :return: None
+    """
     numberOfNodes = graph.getNumberOfNodes()
     numberOfEdges = graph.getNumberOfEdges()
 
@@ -54,6 +73,11 @@ def writeGraphToFile(graph, filename):
 
 
 def displayGraphNodes(graph):
+    """
+    Prints the number of nodes in the graph and lists each node
+    :param graph: the graph
+    :return: None
+    """
     numberOfNodes = graph.getNumberOfNodes()
     textToPrint = f"Number of nodes: {numberOfNodes}\n\n"
     for node in graph.parseNodes():
@@ -61,6 +85,11 @@ def displayGraphNodes(graph):
     print(textToPrint)
 
 def displayGraphEdges(graph):
+    """
+    Prints the number of edges in the graph and details of each edge (id, source and destination) and its cost
+    :param graph: the graph
+    :return: None
+    """
     numberOfEdges = graph.getNumberOfEdges()
     textToPrint = f"Number of edges: {numberOfEdges}\n\n"
     for edge in graph.parseEdges():
@@ -71,8 +100,11 @@ def displayGraphEdges(graph):
 
 
 
+
+# Visual functions
+
 def initialMenu(graph):
-    option = int(input("INITIAL MENU\nChoose an option:\n1.Read from file\n2.Create random graph\n"))
+    option = int(input(" >> INITIAL MENU <<\nChoose an option:\n1.Read from file\n2.Create random graph\n"))
     if option == 1:
         parseFile("graph1k.txt", graph)
     elif option == 2:
@@ -107,6 +139,3 @@ def start():
     menu(graph)
 
 start()
-
-
-# TODO specifications
